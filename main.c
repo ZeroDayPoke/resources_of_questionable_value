09e7bf553f08e9f6fa4648b43e654918cce26dcf
Chris Stamper <cstampok@gmail.com>
2022-10-28 00:40:03 -0500

    blah

--- main.c
+++ main.c
@@ -1,15 +1,11 @@
+#include "main.h"
 #include <stdio.h>
-#include "header.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    print_all("xxicfss", 3, 'B', 3.333, NULL, "test");
-    print_all("cccic", 102, 116, 104, 1, 115);
-    print_all(NULL);
-    return (0);
+	int retval;
+
+	retval = _printf("%%", 33, "test", 505, 'a');
+	printf("%d", retval);
+	return (0);
 }
