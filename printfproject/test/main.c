0473a11ee6ba9a40ffa12ed10910fc64d38eaf59
Chris Stamper <cstampok@gmail.com>
2022-10-31 14:28:27 -0500

    blah test

--- printfproject/test/main.c
+++ printfproject/test/main.c
@@ -6,8 +6,8 @@ int main(void)
 {
 	int retVal1 = 0, retVal2 = 0;
 
-	retVal1 = _printf("%s", "test");
-	retVal2 = printf("%s", "test");
-	printf("\n%d%d\n", retVal1, retVal2);
+	retVal1 = _printf("this thing %s is %d %c long %%%d%d%d", NULL, INT_MIN, 65, 45, 22);
+	retVal2 = printf("this thing %s is %d %c long %%%d%d%d", NULL, INT_MIN, 65, 45, 22);
+	printf("\nfrom custom printf: %d from our printf: %d\n", retVal1, retVal2);
 	return (0);
 }
